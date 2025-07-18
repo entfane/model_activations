2be35ede2b76fe98e000a5113c12e02d4c699e13
entfane <nik77kez@gmail.com>
2025-07-18 22:04:38 +0200

    created basic activation analyzer that records all resid_mid activations

--- src/utils.py
+++ src/utils.py
@@ -1,28 +1,18 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from transformers.models.llama.modeling_llama import LlamaDecoderLayer
+class ActivationAnalyzer():
+
+    def __init__(self):
+        self.activations = {}
+
+    def get_activation(self, idx):
+            def hook(module, input, output):
+                self.activations[idx] = output
+            return hook
+
+    def attach_forward_hooks(self, model):
+        index = 0
 
 
-model = AutoModelForCausalLM.from_pretrained("HuggingFaceTB/SmolLM2-135M")
-tokenizer = AutoTokenizer.from_pretrained("HuggingFaceTB/SmolLM2-135M")
+        for layer in model.model.layers:
+            layer.self_attn.register_forward_hook(self.get_activation(index))
+            index += 1
 
-index = 0
-resids = {}
-
-
-def get_activation(idx):
-    def hook(module, input, output):
-        resids[idx] = output
-    return hook
-
-
-for layer in model.model.layers:
-    h = layer.self_attn.register_forward_hook(get_activation(index))
-    index += 1
-
-prompt = "Give me a short introduction to large language model."
-model_inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
-model.generate(
-    **model_inputs,
-    max_new_tokens=1
-)
-print(resids)
